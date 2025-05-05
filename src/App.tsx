facafa2e103ad5fc18be364a2f7ced80fbb7bc27
nao-United92 <naoyauver@icloud.com>
2025-05-06 00:56:30 +0900

    add newNote function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    const newNote = {
+      id: Date.now(),
+      title: '新しいノート',
+      content: 'ノートの内容です。',
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
